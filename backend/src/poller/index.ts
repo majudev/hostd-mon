99e14c8a075c72342b3aa5985de3537fc170f18a
majudev <maju@majudev.net>
2023-09-02 20:33:38 +0200

    Added performance metrics for poller

--- backend/src/poller/index.ts
+++ backend/src/poller/index.ts
@@ -1,17 +1,21 @@
 import spawnExtramonWorker from './extramon-worker';
+import {performance} from 'perf_hooks';
 
 var extramonWorkerFlag = false;
 const extramonWorkerInterval = 60;
+var workerSpawned = performance.now();
 
 function extramonWorkerScheduler(){
     setTimeout(() => {
         extramonWorkerScheduler();
         if(!extramonWorkerFlag){
             console.log('Spawning new ExtramonWorker');
+            workerSpawned = performance.now();
 
             extramonWorkerFlag = true;
             const worker = spawnExtramonWorker();
             worker.on('message', (result) => {
+                console.log('ExtramonWorker finished it\'s work in ' + (performance.now() - workerSpawned).toFixed(3) + 'ms');
                 extramonWorkerFlag = false;
             });
             worker.on('error', (error) => {
