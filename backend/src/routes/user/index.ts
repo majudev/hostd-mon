a8f8705ed93f82a28e4f4c30185c6eccf6327646
majudev <maju@majudev.net>
2023-09-20 15:16:58 +0200

    Add .../alerts endpoints and new DB fields

--- backend/src/routes/user/index.ts
+++ backend/src/routes/user/index.ts
@@ -5,6 +5,63 @@ import { PrismaClient } from '@prisma/client'
 const router = Router();
 const prisma = new PrismaClient();
 
+router.get('/:id', async (req: Request, res: Response) => {
+    if(!res.locals.authenticated){
+        res.status(401).end();
+        return;
+    }
+
+    const userId: number = parseInt(req.params.id);
+
+    if(Number.isNaN(userId)) {
+        res.status(400).json({
+            status: "error",
+            message: "please provide userId",
+        });
+        return;
+    }
+
+    // User can only view his hosts, admin can view everything
+    if(userId != res.locals.auth_user.userId && !res.locals.auth_user.admin){
+        res.status(403).json({
+            status: "error",
+            message: "you don't have permissions to view this userId",
+        }).end();
+        return;
+    }
+
+    const user = await prisma.user.findFirst({
+        where: {
+            id: userId,
+        },
+        select: {
+            id: true,
+            name: true,
+            email: true,
+            admin: true,
+            alertEmail: true,
+            alertPhoneNumber: true,
+            globallyDisableEmailAlerts: true,
+            globallyDisablePhoneAlerts: true,
+            Hosts: {
+                select: {
+                    id: true,
+                },
+            },
+        },
+    });
+
+    if(user === null){
+        res.status(404).json({
+            status: "error",
+            message: "user with id " + userId + " not found",
+        });
+        return;
+    }
+
+    res.status(200).json(user).end();
+});
+
 router.get('/:id/hosts', async (req: Request, res: Response) => {
     if(!res.locals.authenticated){
         res.status(401).end();
@@ -56,6 +113,52 @@ router.get('/:id/hosts', async (req: Request, res: Response) => {
     }
 
     res.status(200).json(user.Hosts).end();
+});
+
+router.get('/:id/alerts', async (req: Request, res: Response) => {
+    if(!res.locals.authenticated){
+        res.status(401).end();
+        return;
+    }
+
+    const userId: number = parseInt(req.params.id);
+
+    if(Number.isNaN(userId)) {
+        res.status(400).json({
+            status: "error",
+            message: "please provide userId",
+        });
+        return;
+    }
+
+    // User can only view his hosts, admin can view everything
+    if(userId != res.locals.auth_user.userId && !res.locals.auth_user.admin){
+        res.status(403).json({
+            status: "error",
+            message: "you don't have permissions to view this userId",
+        }).end();
+        return;
+    }
+
+    const alerts = await prisma.alert.findMany({
+        where: {
+            userId: userId,
+        },
+        select: {
+            id: true,
+            timestamp: true,
+            message: true,
+            sentTo: true,
+            Host: {
+                select: {
+                    id: true,
+                    name: true,
+                }
+            }
+        },
+    });
+
+    res.status(200).json(alerts).end();
 });
 
 export default router;
