b18b1d4a08a640412b564d7a11bb15a0da91b991
majudev <maju@majudev.net>
2023-09-20 23:29:11 +0200

    Fix /api/user/:userId PATCH

--- backend/src/routes/host/index.ts
+++ backend/src/routes/host/index.ts
@@ -41,17 +41,17 @@ router.post('/new', async (req: Request, res: Response) => {
         return;
     }
 
-    if(await prisma.host.count({ where: { rhpAddress: request.rhpAddress } }) > 0){
+    if(request.rhpAddress !== null && await prisma.host.count({ where: { rhpAddress: request.rhpAddress } }) > 0){
         fail_duplicate_entry(res, "rhpAddress", null);
         return;
     }
 
-    if(await prisma.host.count({ where: { rhpPubkey: request.rhpPubkey } }) > 0){
+    if(request.rhpPubkey !== null && await prisma.host.count({ where: { rhpPubkey: request.rhpPubkey } }) > 0){
         fail_duplicate_entry(res, "rhpPubkey", null);
         return;
     }
 
-    if(await prisma.host.count({ where: { rhpPubkey: request.extramonPubkey } }) > 0){
+    if(request.extramonPubkey !== null && await prisma.host.count({ where: { rhpPubkey: request.extramonPubkey } }) > 0){
         fail_duplicate_entry(res, "extramonPubkey", null);
         return;
     }
