9f12f49f1411cb32dac2354c97f2a95051970a74
Filip Korus <filipkorus123@gmail.com>
2023-10-04 17:08:24 +0200

    login with google

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,9 +1,19 @@
 import axios from 'axios';
 import config from '@/config';
+import {useNavigate} from 'react-router-dom';
 
 const api = axios.create({
 	baseURL: config.API_URL,
 	withCredentials: true
+});
+
+api.interceptors.response.use(res => res, async error => {
+	if (error.response.status === 401) {
+		// alert('Please log in');
+		// window.location.href = '/login';
+	}
+
+	return Promise.reject(error);
 });
 
 export default api;
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,18 +1,22 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import {HostDmonContext, useHostDmon} from '@/context/HostDmonContext.tsx';
-import {useNavigate} from 'react-router-dom';
+import {useNavigate, useParams, useSearchParams} from 'react-router-dom';
 import Container from '@mui/material/Container';
 import CssBaseline from '@mui/material/CssBaseline';
-import { Box, Button, TextField, Typography} from '@mui/material';
-import {login} from '@/api/auth';
-import User from '@/types/User.ts';
-import {useForm} from '@/hooks/useForm.ts';
+import {Box, Button, Typography} from '@mui/material';
+import config from '@/config';
+import api from '@/api';
 
 const Login: React.FC = () => {
 	const navigate = useNavigate();
 	const {currentUser, setCurrentUser} = useHostDmon() as HostDmonContext;
 
-	const [isLoading, setIsLoading] = useState<boolean>(false);
+	const [searchParams, setSearchParams] = useSearchParams();
+	const status = searchParams.get('status');
+
+	if (status === 'success') {
+		api.get('/user/me').then(({data}) => console.log(data))
+	}
 
 	useEffect(() => {
 		if (currentUser) {
@@ -20,29 +24,8 @@ const Login: React.FC = () => {
 		}
 	}, [currentUser]);
 
-	const {formData, handleInputChange} = useForm({
-		email: 'nobody@all', // TODO: replace with empty string in production
-		password: ''
-	});
-
-	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-		event.preventDefault();
-		setIsLoading(true);
-
-		login(formData).then(res => {
-			setIsLoading(false);
-
-			if (res?.status === 'success') {
-				return setCurrentUser(res?.data);
-			}
-		}).catch(error => {
-			console.error(error);
-			const {data} = error?.response;
-			if (data == null) return;
-
-			alert(data?.message ?? error ?? 'Server error');
-			setIsLoading(false);
-		});
+	const handleGoogleLogin = () => {
+		window.location.href = `${config.API_URL}/auth/google`; // Redirects to the backend route for Google OAuth login
 	};
 
 	return <Container component="main" maxWidth="xs">
@@ -58,41 +41,11 @@ const Login: React.FC = () => {
 			<Typography component="h1" variant="h5">
 				Log in
 			</Typography>
-			<Box component="form" onSubmit={handleSubmit} noValidate sx={{mt: 1}}>
-				<TextField
-					margin="normal"
-					required
-					fullWidth
-					label="Email Address"
-					name="email"
-					onChange={handleInputChange}
-					value={formData.email}
-					autoFocus
-				/>
-				<TextField
-					margin="normal"
-					// required // TODO: uncomment in production
-					fullWidth
-					label="Password"
-					name="password"
-					onChange={handleInputChange}
-					value={formData.password}
-					type="password"
-				/>
-				<Button
-					type="submit"
-					fullWidth
-					variant="contained"
-					disabled={isLoading}
-					sx={{mt: 3, mb: 2}}
-				>
-					Log In
+
+			<Box sx={{mt: 1}}>
+				<Button onClick={handleGoogleLogin}>
+					with Google
 				</Button>
-				{/*<Grid item>*/}
-				{/*	<Link href="#" variant="body2">*/}
-				{/*		{"Don't have an account? Sign Up"}*/}
-				{/*	</Link>*/}
-				{/*</Grid>*/}
 			</Box>
 		</Box>
 	</Container>;
